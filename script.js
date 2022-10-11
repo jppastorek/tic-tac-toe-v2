cc400cdf84a2afdf2a6d73a44f16d899486f7102
jppastorek <jppastorek@gmail.com>
2022-10-10 21:43:22 -0500

    Added the logic to check for a winner. Need to find a way to display the winning message and restart the game. And include a draw.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const board = document.getElementById("boardContainer");
 const button = document.getElementById("restartButton");
 const cells = document.getElementsByClassName("cell");
-const xes = document.getElementsByClassName("x");
+const winningMessage = document.querySelector("winning-message");
 
 
 //player objects
@@ -39,7 +39,7 @@ const gameBoard = {
                         player2.active = false;
                         player1.active = true;
                     }
-                    displayController.checkForWin;
+                    displayController.checkForWin();
                 }
             })
 
@@ -66,7 +66,37 @@ const gameBoard = {
 
 const displayController = {
     checkForWin: function() {
-        //some code
+        let cell = gameBoard.boardList;
+        cell0 = cell[0].classList.toString();
+        cell1 = cell[1].classList.toString();
+        cell2 = cell[2].classList.toString();
+        cell3 = cell[3].classList.toString();
+        cell4 = cell[4].classList.toString();
+        cell5 = cell[5].classList.toString();
+        cell6 = cell[6].classList.toString();
+        cell7 = cell[7].classList.toString();
+        cell8 = cell[8].classList.toString();
+        //check the rows
+        if (cell0 === cell1 && cell1 === cell2 && cell0 != "empty") {
+            console.log("win");
+        } else if (cell3 === cell4 && cell4 === cell5 && cell3 != "empty") {
+            console.log("win");
+        } else if (cell6 === cell7 && cell7 === cell8 && cell6 != "empty") {
+            console.log("win");
+            //check the columns
+        } else if (cell0 === cell3 && cell3 === cell6 && cell0 != "empty") {
+            console.log("win");
+        } else if (cell1 === cell4 && cell4 === cell7 && cell1 != "empty") {
+            console.log("win");
+        } else if (cell2 === cell5 && cell5 === cell8 && cell2 != "empty") {
+            console.log("win");
+            //check the crosses
+        } else if (cell0 === cell4 && cell4 === cell8 && cell0 != "empty") {
+            console.log("win");
+        } else if (cell2 === cell4 && cell4 === cell8 && cell2 != "empty") {
+            console.log("win");
+        }
+
     },
     checkForDraw: function() {
         //some code
