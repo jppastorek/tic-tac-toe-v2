a458b0efc82aa5e777bf812f45853d68b93d2ecd
jppastorek <jppastorek@gmail.com>
2022-10-11 22:06:02 -0500

    Refactored. Moved all DOM changes into display controller and all game logic into the gameboard. All that is left to do is the checkForDraw

--- script.js
+++ script.js
@@ -1,63 +1,93 @@
 //DOM elements
 
-const board = document.getElementById("boardContainer");
+const boardContainer = document.getElementById("boardContainer");
 const button = document.getElementById("restartButton");
 const cells = document.getElementsByClassName("cell");
-const winningMessage = document.querySelector("winning-message");
+const winningMessage = document.getElementById("winning-message");
+const messageText = document.getElementById("message-text");
+
+
 
 
 //player objects
 
-const createPlayer = (name, marker, active) => ({ name, marker, active });
-const player1 = createPlayer('player1', 'x', true);
-const player2 = createPlayer('player2', 'o', false);
+const player1 = { name: 'player1', marker: 'x', active: true };
+const player2 = { name: 'player2', marker: 'o', active: false };
+
 
 //game board
 
 const gameBoard = {
-    create: function() {
-        //this will create a new board with 9 divs, labeling each with the index as its data label
-        board.innerHTML = '';
-        let index = 0;
-        for (i = 0; i < 9; i++) {
-            let div = document.createElement("div");
-            div.classList.add("empty");
-            // div.setAttribute("data", index);  do I even need this?
-            board.appendChild(div);
-            this.boardList.push(div);
-
-            //should this part be in the display controller?
-            div.addEventListener("click", () => {
-                if (div.classList.contains("empty")) {
-                    div.classList.remove("empty");
-                    if (player1.active) {
-                        div.classList.add("x");
-                        player1.active = false;
-                        player2.active = true;
-                    } else {
-                        div.classList.add("o");
-                        player2.active = false;
-                        player1.active = true;
-                    }
-                    displayController.checkForWin();
-                }
-            })
-
-            index++;
+    board: [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ],
+    checkForWin: function() {
+        const board = this.board;
+        //check rows
+        if (board[0][0] === board[0][1] && board[0][1] === board[0][2] && board[0][0] != '') {
+            this.declareWinner(board[0][0]);
+        } else if (board[1][0] === board[1][1] && board[1][1] === board[1][2] && board[1][0] != '') {
+            this.declareWinner(board[0][0]);
+        } else if (board[2][0] === board[2][1] && board[2][1] === board[2][2] && board[2][0] != '') {
+            this.declareWinner(board[2][0]);
+            //check columns
+        } else if (board[0][0] === board[1][0] && board[1][0] === board[2][0] && board[0][0] != '') {
+            this.declareWinner(board[0][0]);
+        } else if (board[0][1] === board[1][1] && board[1][1] === board[2][1] && board[0][1] != '') {
+            this.declareWinner(board[0][1]);
+        } else if (board[0][2] === board[1][2] && board[1][2] === board[2][2] && board[0][2] != '') {
+            this.declareWinner(board[0][2]);
+            //check crosses
+        } else if (board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[0][0] != '') {
+            this.declareWinner(board[0][0]);
+        } else if (board[2][0] === board[1][1] && board[1][1] === board[0][2] && board[0][2] != '') {
+            this.declareWinner(board[0][2]);
         }
     },
-    boardList: [], //not sure what to do with this. used to store the list of divs
-    winningCombos: [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 8]
-    ]
+    checkForDraw: function() {
+        //some code
+    },
+    declareWinner: function(mark) {
+        displayController.showMessage(mark);
+    },
+    declareDraw: function() {
+        console.log(`It's a draw.`);
+    },
+    updateBoard: function(cellNumber, mark) {
+        if (cellNumber === "0") {
+            this.board[0][0] = mark;
+        } else if (cellNumber === "1") {
+            this.board[0][1] = mark;
+        } else if (cellNumber === "2") {
+            this.board[0][2] = mark;
+        } else if (cellNumber === "3") {
+            this.board[1][0] = mark;
+        } else if (cellNumber === "4") {
+            this.board[1][1] = mark;
+        } else if (cellNumber === "5") {
+            this.board[1][2] = mark;
+        } else if (cellNumber === "6") {
+            this.board[2][0] = mark;
+        } else if (cellNumber === "7") {
+            this.board[2][1] = mark;
+        } else if (cellNumber === "8") {
+            this.board[2][2] = mark;
+        }
+        console.log(this.board);
+    },
+    newGame: function() {
+        this.board = [
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', '']
+        ];
+        player1.active = true;
+        player2.active = false;
+    }
 }
+
 
 
 //TODO update the current state of the board with new classes (x or o) or and/or use the data
@@ -65,43 +95,50 @@ const gameBoard = {
 //display controller
 
 const displayController = {
-    checkForWin: function() {
-        let cell = gameBoard.boardList;
-        cell0 = cell[0].classList.toString();
-        cell1 = cell[1].classList.toString();
-        cell2 = cell[2].classList.toString();
-        cell3 = cell[3].classList.toString();
-        cell4 = cell[4].classList.toString();
-        cell5 = cell[5].classList.toString();
-        cell6 = cell[6].classList.toString();
-        cell7 = cell[7].classList.toString();
-        cell8 = cell[8].classList.toString();
-        //check the rows
-        if (cell0 === cell1 && cell1 === cell2 && cell0 != "empty") {
-            console.log("win");
-        } else if (cell3 === cell4 && cell4 === cell5 && cell3 != "empty") {
-            console.log("win");
-        } else if (cell6 === cell7 && cell7 === cell8 && cell6 != "empty") {
-            console.log("win");
-            //check the columns
-        } else if (cell0 === cell3 && cell3 === cell6 && cell0 != "empty") {
-            console.log("win");
-        } else if (cell1 === cell4 && cell4 === cell7 && cell1 != "empty") {
-            console.log("win");
-        } else if (cell2 === cell5 && cell5 === cell8 && cell2 != "empty") {
-            console.log("win");
-            //check the crosses
-        } else if (cell0 === cell4 && cell4 === cell8 && cell0 != "empty") {
-            console.log("win");
-        } else if (cell2 === cell4 && cell4 === cell6 && cell2 != "empty") {
-            console.log("win");
-        }
+    create: function() {
+        //this will create a new board with 9 divs, labeling each with the index as its data label
+        boardContainer.innerHTML = '';
+        let index = 0;
+        for (i = 0; i < 9; i++) {
+            let div = document.createElement("div");
+            div.classList.add("empty");
+            div.setAttribute("data-cellNumber", index);
+            boardContainer.appendChild(div);
+            div.addEventListener("click", () => {
+                if (div.classList.contains("empty")) {
+                    div.classList.remove("empty");
+                    if (player1.active) {
+                        div.classList.add("x");
+                        player1.active = false;
+                        player2.active = true;
+                        gameBoard.updateBoard(div.getAttribute("data-cellNumber"), "x");
+                    } else {
+                        div.classList.add("o");
+                        player2.active = false;
+                        player1.active = true;
+                        gameBoard.updateBoard(div.getAttribute("data-cellNumber"), "o");
+                    }
+                    gameBoard.checkForWin();
+                }
+            })
 
+            index++;
+        }
     },
-    checkForDraw: function() {
-        //some code
+    showMessage: function(winner) {
+        messageText.innerText = `${winner.toUpperCase()} wins!`
+        winningMessage.classList.add("show");
+    },
+    restart: function() {
+        winningMessage.classList.remove("show");
     }
 }
 
 
-gameBoard.create();
+button.addEventListener("click", () => {
+    displayController.restart();
+    displayController.create();
+    gameBoard.newGame();
+});
+
+displayController.create();
